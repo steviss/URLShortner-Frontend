6ee3877a051f796be2b6f5c36e7ddcc14a1a5246
Stefan Stevic <steviss@gmail.com>
2020-12-01 15:48:00 +0100

    https://code.visualstudio.com/docs/typescript/typescript-compiling#_using-the-workspace-version-of-typescript
    https://github.com/facebook/create-react-app/issues/10144
    
    ^ Fix for the newest version of react 17 and "react-jsx" tag.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Container, Drawer, Grid } from '@material-ui/core';
+import React from 'react';
+import { Container, Drawer, Grid, ThemeProvider } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
 import Routes from '@routes';
 import { StoreProvider } from '@stores';
-import React from 'react';
 import { drawerStyle, wrapperStyle } from '@styles';
 import './App.css';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#757ce8',
+            main: '#3f50b5',
+            dark: '#002884',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
+    },
+});
 
 const App: React.FC = () => {
     var isIE11 = !!window.MSInputMethodContext;
@@ -12,14 +30,16 @@ const App: React.FC = () => {
     const drawerCSS = drawerStyle();
     return (
         <StoreProvider>
-            <Drawer anchor="right" classes={drawerCSS} />
-            <main className={wrapperCSS.root}>
-                <Container maxWidth="lg" className={wrapperCSS.container}>
-                    <Grid container spacing={3}>
-                        <Routes />
-                    </Grid>
-                </Container>
-            </main>
+            <ThemeProvider theme={theme}>
+                <Drawer anchor="right" classes={drawerCSS} />
+                <main className={wrapperCSS.root}>
+                    <Container maxWidth="lg" className={wrapperCSS.container}>
+                        <Grid container spacing={3}>
+                            <Routes />
+                        </Grid>
+                    </Container>
+                </main>
+            </ThemeProvider>
         </StoreProvider>
     );
 };
